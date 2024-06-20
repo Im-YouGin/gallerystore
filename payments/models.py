30e688309d7e20acc34bbfe978b2a38933b7116b
Reyek <zreek2001@gmail.com>
2024-06-21 00:34:57 +0300

    Fix models fields

--- payments/models.py
+++ payments/models.py
@@ -8,7 +8,7 @@ from payments.constants import PaymentStatus
 
 class Payment(TimestampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    order_id = models.CharField(max_length=20)
+    order_id = models.CharField(max_length=36)
     status = models.CharField(max_length=20, choices=PaymentStatus.choices(), default=PaymentStatus.PENDING)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3)

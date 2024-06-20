5039f6a34080bf7c982612c821fa79faceba281f
Reyek <zreek2001@gmail.com>
2024-06-21 00:28:18 +0300

    Add cors

--- GalleryStoreBackend/settings.py
+++ GalleryStoreBackend/settings.py
@@ -48,11 +48,13 @@ INSTALLED_APPS = [
     "payments.apps.PaymentsConfig",
     # 3rd party apps
     "rest_framework",
+'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -97,3 +99,5 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
